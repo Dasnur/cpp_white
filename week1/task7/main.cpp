10c16e4fdd030b429b9ec8fffcf3241fd8d97704
Dasnur <dasnur@DESKTOP-B99EBJQ.localdomain>
2020-02-18 17:41:28 +0300

    task7

---
+++ week1/task7/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    int n = 0;
+    int res = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == 'f')
+        {
+            n++;
+            if (n == 2)
+                res = i;
+        }
+    }
+    if (n == 2)
+        cout << res;
+    if (n == 1)
+        cout << -1;
+    if (n == 0)
+        cout << -2;
+    return 0;
+}
