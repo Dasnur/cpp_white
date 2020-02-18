9316f1e5d858041d49dbd59d6f4a0e8d757a9d89
Dasnur <dasnur@DESKTOP-B99EBJQ.localdomain>
2020-02-18 15:57:30 +0300

    second task

---
+++ week1/task2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n = 3;
+    vector<string> a;
+    string res;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> res;
+        a.push_back(res);
+    }
+    sort(begin(a), end(a));
+    cout << a[0];
+    return 0;
+}
