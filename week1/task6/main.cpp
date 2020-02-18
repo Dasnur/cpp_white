67c1f72996f94195d417c0818048f8806cbff8db
Dasnur <dasnur@DESKTOP-B99EBJQ.localdomain>
2020-02-18 17:30:08 +0300

    task5

---
+++ week1/task6/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    while (a <= b)
+    {
+        if (a % 2 == 0)
+        {
+            cout << a;
+            if (a != b && (b % 2 == 0))
+                cout << ' ';
+        }
+        a++;
+    }
+    return 0;
+}
