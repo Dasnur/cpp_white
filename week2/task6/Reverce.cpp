9f41ae61f7c9d4552cdea8316ade02de8c23b9c4
Dasnur <dasnur@DESKTOP-B99EBJQ.localdomain>
2020-02-19 11:44:06 +0300

    w2t6_v2

--- week2/task6/Reverce.cpp
+++ week2/task6/Reverce.cpp
@@ -2,7 +2,7 @@ void    Reverce(vector<int>& v)
 {
     vector<int> tmp;
 
-    for (i = v.size() - 1; i > -1; i--)
+    for (int i = v.size() - 1; i > -1; i--)
         tmp.push_back(v[i]);
     v = tmp;
 }
