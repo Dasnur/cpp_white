506dbc004231f3176d61d2ff245c388d2228a276
Dasnur <dasnur@DESKTOP-SPUJBBO.localdomain>
2020-02-23 19:35:49 +0300

    w2t17_v6

--- week2/task17/sinonim.cpp
+++ week2/task17/sinonim.cpp
@@ -5,6 +5,62 @@
 #include <set>
 using namespace std;
 
+void	add(map<string, set<string>> &m)
+{
+	string s1;
+    string s2;
+	cin >> s1 >> s2;
+    m[s1].insert(s2);
+}
+
+void 	count(map<string, set<string>> &m)
+{
+	string s1;
+    cin >> s1;
+    int i = 0;
+    for (auto item : m)
+    {
+        int flag = 0;
+        for (auto sino : item.second)
+        {
+            if (sino == s1)
+            {
+                flag = 2;
+                for (auto aaa : m[s1])
+                {
+                    if (item.first == aaa)
+                    flag = 1;
+                }
+            } 
+        }
+        if (flag == 2)
+            i++;
+    }
+    cout << i + m[s1].size() << endl;
+}
+
+void	check(map<string, set<string>> &m)
+{
+	string s1;
+    string s2;
+    int flag = 0;
+    cin >> s1 >> s2;
+    for (auto sino : m[s1])
+    {
+    	if (sino == s2)
+            flag = 1;
+    }
+    for (auto sino : m[s2])
+    {
+        if (sino == s1)
+            flag = 1;
+    }
+    if (flag == 1)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
 int main()
 {
     int q;
@@ -15,58 +71,11 @@ int main()
         string s;
         cin >> s;
         if (s == "ADD")
-        {
-            string s1;
-            string s2;
-            cin >> s1 >> s2;
-            m[s1].insert(s2);
-        }
+			add(m);
         if (s == "COUNT")
-        {
-            string s1;
-            cin >> s1;
-            int i = 0;
-            for (auto item : m)
-            {
-                int flag = 0;
-                for (auto sino : item.second)
-                {
-                    if (sino == s1)
-                    {
-                        flag = 2;
-                        for (auto aaa : m[s1])
-                        {
-                            if (item.first == aaa)
-                                flag = 1;
-                        }
-                    } 
-                }
-                if (flag == 2)
-                    i++;
-            }
-            cout << i + m[s1].size() << endl;
-        }
+			count(m);
         if (s == "CHECK")
-        {
-            string s1;
-            string s2;
-            int flag = 0;
-            cin >> s1 >> s2;
-            for (auto sino : m[s1])
-            {
-                if (sino == s2)
-                    flag = 1;
-            }
-            for (auto sino : m[s2])
-            {
-                if (sino == s1)
-                    flag = 1;
-            }
-            if (flag == 1)
-                cout << "YES" << endl;
-            else
-                cout << "NO" << endl;
-        }
+			check(m);
         q--;
     }
     return 0;
