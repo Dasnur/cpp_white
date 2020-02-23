e6118e4a54e9798f156bc4f8a1037ce459f4f95b
Dasnur <dasnur@DESKTOP-SPUJBBO.localdomain>
2020-02-24 00:25:25 +0300

    w2t17_v13

--- week2/task17/sinonim.cpp
+++ week2/task17/sinonim.cpp
@@ -5,46 +5,28 @@
 #include <set>
 using namespace std;
 
-int		cc(vector<string> &v, string s)
-{
-	for (auto item : v)
-	{
-		if (item == s)
-			return 1;
-	}
-	return 0;
-}
-
-void	add(map<string, vector<string>> &m)
+void	add(map<string, set<string>> &m)
 {
 	string s1;
     string s2;
 	cin >> s1 >> s2;
-	m[s1].push_back(s2);
+	m[s1].insert(s2);
+	m[s2].insert(s1);
 }
 
-void 	count(map<string, vector<string>> &m)
+void 	count(map<string, set<string>> &m)
 {
 	string s1;
     cin >> s1;
-    int i = 0;
-	set<string> tmp;
-	for (auto item : m)
-	{
-		if (cc(item.second, s1))
-			tmp.insert(item.first);
-	}
-	tmp.insert(m[s1].begin(), m[s1].end());
-    cout << tmp.size() << endl;
+	cout << m[s1].size() << endl;
 }
 
-void	check(map<string, vector<string>> &m)
+void	check(map<string, set<string>> &m)
 {
 	string s1;
     string s2;
-    int flag = 0;
     cin >> s1 >> s2;
-    if (cc(m[s1], s2) || cc(m[s2], s1))
+    if (m[s1].count(s2))
         cout << "YES" << endl;
     else
         cout << "NO" << endl;
@@ -53,7 +35,7 @@ void	check(map<string, vector<string>> &m)
 int main()
 {
     int q;
-    map<string, vector<string>> m;
+    map<string, set<string>> m;
     cin >> q;
     while (q > 0)
     {
